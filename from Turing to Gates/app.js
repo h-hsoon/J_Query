52bbf68c4665368a207e856ea22acb68c1c93050
Hanna <hsoonhanna0@gmail.com>
2024-05-24 13:01:48 +0200

    from Turing to Gates

---
+++ from Turing to Gates/app.js
@@ -0,0 +1,9 @@
+$(".div1").click(function() {
+    $(".div1 img").toggle();
+})
+$(".div2").click(function() {
+    $(".div2 img").toggle();
+})
+$(".div3").click(function() {
+    $(".div3 img").toggle();
+})
